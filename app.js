c0650a7284394cdd6f318d59031db1daadb8289e
Gillio78 <gilles.brueziere@sfr.fr>
2025-03-29 13:16:47 +0100

    Récupération cinema au clic

--- app.js
+++ app.js
@@ -62,10 +62,11 @@ app.get('/getCinemas', (req, res) => {
 app.get('/getFilmIds', (req, res) => {
     console.log('Requête reçue sur /getFilmIds'); // Log pour vérifier que la route est appelée
     const query = `
-       SELECT DISTINCT film.titre 
-        FROM films_projetes
-        INNER JOIN film ON films_projetes.film = film.id;
-        `;
+        SELECT DISTINCT film.titre 
+FROM films_projetes
+INNER JOIN film ON films_projetes.film = film.id;
+
+    `;
     db.query(query, (err, results) => {
         if (err) {
             console.error('Erreur lors de la récupération des IDs des films :', err);
